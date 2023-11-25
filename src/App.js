03bed0c9bd7e7442196a6e3ba7e21c76c802ceb1
abroroo <abroriskandarov99@gmail.com>
2023-11-25 14:00:21 +0900

    Basic view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-xl font-bold border p-2 absolute top-20 left-[40%]'>Footprint Calculator</h1>
     </div>
   );
 }
